d3b9b1cc3efe26e92a08515ab2c4f163dcb57ed4
Hengji <hengji.liu@outlook.com>
2017-05-04 23:59:55 +1000

    dao update

--- JobServices/src/main/java/au/edu/unsw/soacourse/jobs/dao/PostingsDao.java
+++ JobServices/src/main/java/au/edu/unsw/soacourse/jobs/dao/PostingsDao.java
@@ -1,7 +1,6 @@
 package au.edu.unsw.soacourse.jobs.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,43 +8,76 @@ import java.sql.Statement;
 
 import au.edu.unsw.soacourse.jobs.model.Posting;
 
-public class PostingsDao implements DAO<Posting> {
-    
-	private Connection conn;
+public class PostingsDAO {
 
-	public PostingsDao() {
-	    try {
-			Class.forName("org.sqlite.JDBC");
-			String url = "jdbc:sqlite:db.sqlite";
-			this.conn = DriverManager.getConnection(url);
-		} catch (ClassNotFoundException|SQLException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Override
-	public Posting getOne(String id) {
+	public Posting findById(String id) {
 		String sql = "SELECT jobId, companyName, salaryRate, positionType, location, descriptions, status" //
 				+ " FROM postings" //
 				+ " WHERE jobId = ?;";
-
+		
+		Connection conn = DBUtil.getConnection();
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, id);
 			ResultSet rs = pstmt.executeQuery();
-			Posting p = new Posting();
-			p.setJobId(rs.getString("jobId"));
-			p.setCompanyName(rs.getString("companyName"));
-			p.setSalaryRate(rs.getString("salaryRate"));
-			p.setPositionType(rs.getString("positionType"));
-			p.setLocation(rs.getString("location"));
-			p.setDescriptions(rs.getString("descriptions"));
-			p.setStatus(rs.getInt("status"));
-			return p;
+			if (rs.next()) {
+				Posting p = new Posting();
+				p.setJobId(rs.getString("jobId"));
+				p.setCompanyName(rs.getString("companyName"));
+				p.setSalaryRate(rs.getString("salaryRate"));
+				p.setPositionType(rs.getString("positionType"));
+				p.setLocation(rs.getString("location"));
+				p.setDescriptions(rs.getString("descriptions"));
+				p.setStatus(rs.getString("status"));
+				
+				return p;
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
+		}finally {
+			DBUtil.closeConnection(conn);
 		}
 		
+		return null;
+	}
+
+	public int insert(Posting obj) {
+		String sql = "INSERT INTO postings" //
+				+ " (companyName, salaryRate, positionType, location, descriptions, status)" //
+				+ " VALUES (?,?,?,?,?,?)";
+		
+		Connection conn = DBUtil.getConnection();
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			pstmt.setString(1, obj.getCompanyName());
+			pstmt.setString(2, obj.getSalaryRate());
+			pstmt.setString(3, obj.getPositionType());
+			pstmt.setString(4, obj.getLocation());
+			pstmt.setString(5, obj.getDescriptions());
+			pstmt.setString(6,  obj.getStatus());
+			int affectedRows = pstmt.executeUpdate();
+			if (0 == affectedRows)
+				return 0 ;
+	        ResultSet generatedKeys = pstmt.getGeneratedKeys();
+	        if (generatedKeys.next())
+            	return generatedKeys.getInt(1);
+	        else
+				return 0;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			DBUtil.closeConnection(conn);
+		}
+		return 0;
+	}
+
+	public Posting delete(Posting t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Posting update(Posting t) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
