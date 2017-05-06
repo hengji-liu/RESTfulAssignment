18bd6045ad922fc5c4d692dea2633886697d7e34
radengoots <raden.goots@yahoo.com>
2017-05-07 00:34:14 +1000

    implement polling webservice, still no way to update final choice if votes not empty

--- PollingServices/src/main/java/au/edu/unsw/soacourse/polling/Polling.java
+++ PollingServices/src/main/java/au/edu/unsw/soacourse/polling/Polling.java
@@ -1,15 +1,27 @@
 package au.edu.unsw.soacourse.polling;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
@@ -27,15 +39,28 @@ public class Polling {
 	private Dao<Poll, String> pollDao;
 	private Dao<Vote, String> voteDao;
 	protected ConnectionSource connectionSource;
+	
+	@Context
+	UriInfo uri;
 
     public Dao<Poll, String> getPollDao() {
+    	try {
+			connectionSource = new JdbcConnectionSource(DATABASE_URL);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+    	
     	if (pollDao == null) {
     		try {
-				connectionSource = new JdbcConnectionSource(DATABASE_URL);
+				
 				pollDao = DaoManager.createDao(connectionSource, Poll.class);
 				if (!pollDao.isTableExists()) {
 					TableUtils.createTable(connectionSource, Poll.class);
-				}
+				} 
+//				else {
+//					TableUtils.dropTable(connectionSource, Poll.class, true);
+//				}
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -45,13 +70,22 @@ public class Polling {
 	}
 
 	public Dao<Vote, String> getVoteDao() {
+    	try {
+			connectionSource = new JdbcConnectionSource(DATABASE_URL);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+    	
     	if (voteDao == null) {
     		try {
-				connectionSource = new JdbcConnectionSource(DATABASE_URL);
 				voteDao = DaoManager.createDao(connectionSource, Vote.class);
 				if (!voteDao.isTableExists()) {
 					TableUtils.createTable(connectionSource, Vote.class);
 				}
+//				else {
+//				TableUtils.dropTable(connectionSource, Vote.class, true);
+//			}
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -59,34 +93,386 @@ public class Polling {
     	}
 		return voteDao;
 	}
-
-    @GET
-    @Path("/poll/{input}")
-    @Produces("text/plain")
-    public String getPoll(@PathParam("input") String input) {
-        try {
-			return String.valueOf(getPollDao().countOf());
+    
+//    @GET
+//    @Path("/votes/")
+//    @Produces("text/plain")
+//    public String getVotes(@PathParam("input") String input) {
+//        return input;
+//    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Path("/addNewVote")
+    public Response addNewVote(
+    		@FormParam("participantName") String participantName, 
+    		@FormParam("chosenOption") String chosenOption,
+    		@FormParam("pollId") String pollId) {
+    	String voteId = UUID.randomUUID().toString();
+    	try {
+        	Poll poll = getPollDao().queryForId(pollId);
+        	Vote outputVote = new Vote(voteId, participantName, chosenOption, poll);
+			URI outputVoteUri = new URI(uri.getBaseUri() + "polling/vote/" + voteId);
+			
+        	if (getVoteDao().create(outputVote) > 0) 
+        		connectionSource.close();
+				return Response.created(outputVoteUri).build();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-        return input;
+    	
+    	try {
+			connectionSource.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	return Response.serverError().build();
+    }
+    
+    @GET
+    @Path("/vote/{id}")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response getVote(@PathParam("id") String voteId) {
+        try {
+        	Vote outVote = getVoteDao().queryForId(voteId);
+//        	getPollDao().refresh(outVote.getPoll());
+
+        	connectionSource.close();
+        	if (outVote != null)
+        		return Response.ok(outVote).build();
+        	else
+        		return Response.noContent().build();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        try {
+			connectionSource.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        return Response.serverError().build();
+    }
+    
+    @PUT
+    @Path("/vote/edit/{id}")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response updateVote(    		
+    		@PathParam("id") String voteId,
+    		@FormParam("participantName") String participantName, 
+    		@FormParam("chosenOption") String chosenOption,
+    		@FormParam("pollId") String pollId) {
+        try {
+        	Vote updatedVote = getVoteDao().queryForId(voteId);
+        	
+        	if (updatedVote == null) {
+        		connectionSource.close();
+        		return Response.noContent().build();
+        	}
+        	
+        	Poll poll = getPollDao().queryForId(pollId);
+        	
+        	if (poll.getFinalChoice() != null && poll.getFinalChoice() != "") {
+        		connectionSource.close();
+        		return Response.status(412).build();
+        	}
+        	
+        	updatedVote.setChosenOption(chosenOption);
+        	updatedVote.setParticipantName(participantName);
+        	updatedVote.setPoll(poll);
+        	
+        	getVoteDao().update(updatedVote);
+
+        	if (getVoteDao().update(updatedVote) > 0) {
+        		connectionSource.close();
+        		return Response.ok().build();
+        	}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        try {
+			connectionSource.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        return Response.serverError().build();
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Path("/addNewPoll")
+    public Response addNewPoll(
+    		@FormParam("title") String title, 
+    		@FormParam("description") String description,
+    		@FormParam("optionsType") String optionsType,
+    		@FormParam("comments") String comments,
+    		@FormParam("options") String options) {
+    	String pollId = UUID.randomUUID().toString();
+    	try {
+        	Poll outputPoll = new Poll(pollId, title, description, optionsType, options, comments);
+			URI outputPollUri = new URI(uri.getBaseUri() + "polling/poll/" + pollId);
+        	if (getPollDao().create(outputPoll) > 0) 
+        		connectionSource.close();
+				return Response.created(outputPollUri).build();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	try {
+			connectionSource.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	return Response.serverError().build();
+    }
+
+    @GET
+    @Path("/poll/{id}")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response getPoll(@PathParam("id") String pollId) {
+        try {
+        	Poll outPoll = getPollDao().queryForId(pollId);
+        	
+        	if (outPoll != null) {
+            	List<Vote> votes = getVoteDao().queryBuilder().where().eq("poll_id", outPoll).query();
+            	outPoll.setVotes(votes);
+
+            	connectionSource.close();
+        		return Response.ok(outPoll).build();
+        	}
+        	
+    		connectionSource.close();
+    		return Response.noContent().build();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}    	
+        
+        try {
+			connectionSource.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	return Response.serverError().build();
     }
     
     @GET
     @Path("/polls/")
-    @Produces("text/plain")
-    public String getPolls(@PathParam("input") String input) {
-        return input;
+    @Produces("application/json")
+    public Response getPolls() {
+        try {
+        	List<Poll> outPolls = getPollDao().queryForAll();
+        	if (outPolls != null) {
+        		List<Poll> retPolls = new ArrayList<Poll>();
+            	for (Poll poll : outPolls) {
+            		List<Vote> votes = getVoteDao().queryBuilder().where().eq("poll_id", poll.getPollId()).query();
+                	poll.setVotes(votes);
+                	retPolls.add(poll);
+    			}
+            	
+            	connectionSource.close();
+        		return Response.ok().entity(retPolls).build();
+        	}
+        	
+        	connectionSource.close();
+        	return Response.noContent().build();        	
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        try {
+			connectionSource.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	return Response.serverError().build();
     }
     
-    @POST
+    @PUT
+    @Path("/poll/edit/{id}")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response updatePoll(
+    		@PathParam("id") String pollId,
+    		@FormParam("title") String title, 
+    		@FormParam("description") String description,
+    		@FormParam("optionsType") String optionsType,
+    		@FormParam("comments") String comments,
+    		@FormParam("options") String options,
+    		@FormParam("finalChoice") String finalChoice) {
+        try {
+        	Poll updatedPoll = getPollDao().queryForId(pollId);
+        	
+        	if (updatedPoll == null) {
+        		connectionSource.close();
+        		return Response.noContent().build();
+        	}
+        	
+        	List<Vote> votes = getVoteDao().queryBuilder().where().eq("poll_id", updatedPoll).query();
+        	
+        	if (votes.isEmpty()) {        	
+	        	updatedPoll.setComments(comments);
+	        	updatedPoll.setDescription(description);
+	        	updatedPoll.setOptions(options);
+	        	updatedPoll.setOptionType(optionsType);
+	        	updatedPoll.setTitle(title);
+	        	updatedPoll.setFinalChoice(finalChoice);
+	        	
+	        	if (getPollDao().update(updatedPoll) > 0) {
+	            	connectionSource.close();
+	        		return Response.ok().build();
+	        	}
+        	} else {
+        		connectionSource.close();
+        		return Response.status(412).build();
+        	}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        try {
+			connectionSource.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        return Response.serverError().build();
+    }
+    
+    @DELETE
+    @Path("/poll/delete/{id}")
+    public Response deletePoll(@PathParam("id") String pollId) {
+        try {
+        	Poll deletePoll = getPollDao().queryForId(pollId);
+        	
+        	if (deletePoll == null) {
+        		connectionSource.close();
+        		return Response.notAcceptable(null).build();
+        	}
+        	
+        	List<Vote> votes = getVoteDao().queryBuilder().where().eq("poll_id", deletePoll).query();
+        	
+        	if (votes.isEmpty()) {    	
+        		if (getPollDao().delete(deletePoll) > 0) {
+        			connectionSource.close();
+            		return Response.ok().build();
+            	}
+        	} else {
+        		connectionSource.close();
+        		return Response.notAcceptable(null).build();
+        	}
+        	
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        try {
+			connectionSource.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        return Response.serverError().build();
+    }
+    
+    @GET
+    @Path("/polls/search/")
     @Produces("application/json")
-    @Consumes("application/json")
-    @Path("/addNewVote")
-    public Response addNewVote(Vote inputVote) {
-    	String voteId = UUID.randomUUID().toString();
-    	Vote outputVote = new Vote(voteId, inputVote.getParticipantName(), inputVote.getChosenOption());
-        return Response.ok().entity(outputVote).build();
+    public Response searchPolls(
+    		@QueryParam("title") String title,
+    		@QueryParam("description") String description,
+    		@QueryParam("optionType") String optionType,
+    		@QueryParam("comments") String comments) {
+        try {
+        	Poll queryPoll = new Poll();
+        	queryPoll.setTitle(title);
+        	queryPoll.setComments(comments);
+        	queryPoll.setDescription(description);
+        	queryPoll.setOptionType(optionType);
+        	
+        	List<Poll> outPolls = getPollDao().queryForMatching(queryPoll);
+        	
+        	if (!outPolls.isEmpty()) {
+        		List<Poll> retPolls = new ArrayList<Poll>();
+            	for (Poll poll : outPolls) {
+            		List<Vote> votes = getVoteDao().queryBuilder().where().eq("poll_id", poll.getPollId()).query();
+                	poll.setVotes(votes);
+                	retPolls.add(poll);
+    			}
+            	
+            	connectionSource.close();
+        		return Response.ok().entity(retPolls).build();
+        	}
+        	
+        	connectionSource.close();
+        	return Response.noContent().build();  
+        	
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        try {
+			connectionSource.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        return Response.serverError().build();
     }
 }
--- PollingServices/src/main/java/au/edu/unsw/soacourse/polling/beans/Vote.java
+++ PollingServices/src/main/java/au/edu/unsw/soacourse/polling/beans/Vote.java
@@ -1,44 +1,94 @@
 package au.edu.unsw.soacourse.polling.beans;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
+@JsonIgnoreProperties(value = {"Poll"}, ignoreUnknown=true)
 @DatabaseTable(tableName = "votes")
+@XmlRootElement
 public class Vote {
-	@DatabaseField
+	@DatabaseField(id = true)
+	@XmlAttribute
 	private String voteId;
+	
+	@DatabaseField(foreign = true, canBeNull = false)
+	@XmlElement
+	private Poll poll;
+	
 	@DatabaseField
+	@XmlElement(name = "participant-name")
 	private String participantName;
+	
 	@DatabaseField
+	@XmlElement
 	private String chosenOption;
 	
 	public Vote() {
 		
 	}
 	
-	public Vote(String voteId, String participantName, String chosenOption) {
+	public Vote(String voteId, String participantName, String chosenOption, Poll poll) {
 		super();
 		this.voteId = voteId;
 		this.participantName = participantName;
 		this.chosenOption = chosenOption;
+		this.poll = poll;
+	}
+
+	public Poll getPoll() {
+		return poll;
+	}
+
+	public void setPoll(Poll poll) {
+		this.poll = poll;
 	}
 
 	public String getVoteId() {
 		return voteId;
 	}
+	
 	public void setVoteId(String voteId) {
 		this.voteId = voteId;
 	}
+	
 	public String getParticipantName() {
 		return participantName;
 	}
+	
 	public void setParticipantName(String participantName) {
 		this.participantName = participantName;
 	}
+	
 	public String getChosenOption() {
 		return chosenOption;
 	}
+	
 	public void setChosenOption(String chosenOption) {
 		this.chosenOption = chosenOption;
+	}
+	
+	@Override
+	public int hashCode() {
+		return voteId.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (other == null || other.getClass() != getClass()) {
+			return false;
+		}
+		return voteId.equals(((Vote) other).voteId);
+	}
+
+	@Override
+	public String toString() {
+		return "Vote [voteId=" + voteId + ", participantName="
+				+ participantName + ", chosenOption=" + chosenOption + "]";
 	}
 }
