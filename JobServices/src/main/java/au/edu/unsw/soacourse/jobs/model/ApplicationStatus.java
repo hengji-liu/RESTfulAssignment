1439d672a8d1cf23662d64721f8c4c08f27edc26
Hengji <hengji.liu@outlook.com>
2017-05-08 21:49:36 +1000

    added PUT to update application status

--- JobServices/src/main/java/au/edu/unsw/soacourse/jobs/model/ApplicationStatus.java
+++ JobServices/src/main/java/au/edu/unsw/soacourse/jobs/model/ApplicationStatus.java
@@ -3,6 +3,6 @@ package au.edu.unsw.soacourse.jobs.model;
 public class ApplicationStatus {
 	public static final int RECEIVED = 0;
 	public static final int IN_REVIEW = 1;
-	public static final int ACCEPT = 2;
+	public static final int ACCEPTED= 2;
 	public static final int REJECTED = 3;
 }
